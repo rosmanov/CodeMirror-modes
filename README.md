8199cb50682fc6f3567c12c18d300ea6622d7de8
Ruslan Osmanov <rrosmanov@gmail.com>
2013-10-12 23:10:06 +0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 CodeMirror-modes
 ================
 
-CodeMirror modes
+Some CodeMirror<http://codemirror.net> modes that haven't been submitted to the CodeMirror project.
---
+++ bbcode/bbcode.js
@@ -0,0 +1,85 @@
+/**
+* @file bbcode.js
+* @brief BBCode mode for CodeMirror<http://codemirror.net/>
+* @author Ruslan Osmanov <rrosmanov@gmail.com>
+* @version 1.0
+* @date 12.10.2013
+*/
+
+CodeMirror.defineMode("bbcode", function(config) {
+  var settings, reTags, reUnaryTags, reTagsWithVal, reLink, openTags = [], m;
+  
+  settings = {
+    bbCodeTags: "b i u s img quote code list table  tr td",
+    bbCodeUnaryTags: "* :-) hr cut",
+    bbCodeTagsWithVal: "size color url",
+    bbCodeErrors: true
+  };
+
+  if (config.hasOwnProperty("bbCodeTags")) {
+    settings.bbCodeTags = config.bbCodeTags;
+  }
+  if (config.hasOwnProperty("bbCodeUnaryTags")) {
+    settings.bbCodeUnaryTags = config.bbCodeUnaryTags;
+  }
+  if (config.hasOwnProperty("bbCodeTagsWithVal")) {
+    settings.bbCodeTagsWithVal = config.bbCodeTagsWithVal;
+  }
+  if (config.hasOwnProperty("bbCodeErrors")) {
+    settings.bbCodeErrors = config.bbCodeErrors;
+  }
+  if (settings.bbCodeTagsWithVal) {
+    settings.bbCodeTags += " " + settings.bbCodeTagsWithVal;
+  }
+
+  function escapeRegEx(s) {
+    return s.replace(/([\:\-\)\(\*\+\?\[\]])/g, '\\$1');
+  }
+
+  reTags = new RegExp("^\\[(\\/)?(" + escapeRegEx(settings.bbCodeTags).split(" ").join("|") + ")\\]");
+  reUnaryTags = new RegExp("^\\[(" + escapeRegEx(settings.bbCodeUnaryTags).split(" ").join("|") + ")\\]");
+  reTagsWithVal = new RegExp("^\\[(" + escapeRegEx(settings.bbCodeTagsWithVal).split(" ").join("|") + ")(?:[^\\]]*)\\]");
+  reLink = /^(?:https?|s?ftp)\:\/\/[^\s\[\]]+/;
+
+  return {
+    token: function(stream, state) {
+      if (stream.eatSpace()) return null;
+
+      if (stream.match('[', false)) {
+        stream.eatWhile(/[^[]/);
+
+        if (m = stream.match(reTags, true)) {
+          if (settings.bbCodeErrors) {
+            if (m[1] == '/') {
+              if (openTags.length && openTags[openTags.length - 1] != m[2])
+                return "error";
+              openTags.pop(m[2]);
+            } else {
+              openTags.push(m[2]);
+            }
+          }
+          return "tag";
+        }
+
+        if (m = stream.match(reUnaryTags, true)) {
+          return "atom";
+        }
+
+        if (m = stream.match(reTagsWithVal, true)) {
+          if (settings.bbCodeErrors) {
+            openTags.push(m[1]);
+          }
+          return "tag";
+        }
+      } else if (stream.match(reLink, true)) {
+        return "link";
+      }
+
+      stream.next();
+      return null;
+    }
+  };
+});
+
+CodeMirror.defineMIME("text/x-bbcode", "bbcode");
+// vim: et ts=2 sts=2 sw=2
